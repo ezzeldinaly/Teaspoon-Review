c944ca10d85749feffe951803e83460245b6a815
ezzeldinaly <ezzeldinaly@icloud.com>
2024-10-11 14:39:53 +0300

    Update indexScript.js

--- indexScript.js
+++ indexScript.js
@@ -6,17 +6,19 @@ async function updateStoreName() {
         
         const response = await fetch('https://script.google.com/macros/s/AKfycbx7BOwQr9FVPFZiX0KmTfygUTFCG8Uafp-TBg1vcsuvmddseGclHFv4iAoQDjWSCam34w/exec'); // Replace with your Google Apps Script URL
         const data = await response.json();
-
-        
         const store = data.stores.find(s => s.id == storeId);
 
         if (store) {
             
             document.getElementById('store-name').textContent = `Welcome to ${store.storeName}`;
             
-
+            
             const thumbsUpLink = document.getElementById('thumbs-up-link');
             thumbsUpLink.href = `happy.html?store=${storeId}`;
+
+            
+            const thumbsDownLink = document.getElementById('thumbs-down-link');
+            thumbsDownLink.href = `feedback.html?store=${storeId}`;
         } else {
             document.getElementById('store-name').textContent = 'Store not found';
         }
@@ -24,6 +26,5 @@ async function updateStoreName() {
         document.getElementById('store-name').textContent = 'No store specified';
     }
 }
-
 
 window.onload = updateStoreName;
